202b3f3f544a45ec3b2566dc91622418d8339e25
Jan Dvorak <storytellercz@protonmail.com>
2022-06-12 15:53:01 +0200

    Update package.js
    Make things compatible with modern Meteor

--- package.js
+++ package.js
@@ -1,16 +1,16 @@
 Package.describe({
   name: "pfafman:mongo-counter",
   summary: "Atomic counters stored in MongoDB",
-  version: "0.0.6",
+  version: "0.0.8",
   git: "https://github.com/pfafman/meteor-mongo-counter.git"
 });
 
 Package.onUse(function (api) {
-  //api.versionsFrom("METEOR@1.0");
+  api.versionsFrom("METEOR@1.8.1");
   api.use(['coffeescript', 'mongo-livedata'], 'server');
-  
+
   api.addFiles('counter.coffee', 'server');
-  
+
   if (api.export) {
     api.export('incrementCounter', 'server');
     api.export('decrementCounter', 'server');
